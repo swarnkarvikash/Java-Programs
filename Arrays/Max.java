d1ead139de41e558e8178e8ae4e02f2c8610c5d6
swarnkarvikash <vikashswarnkar3@gmail.com>
2024-06-15 23:42:06 +0530

    Added Max file

---
+++ Arrays/Max.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Max {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of element: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter element in Array: ");
+        for(int i =0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 0;
+         for(int i=0;i<n;i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+         }
+         System.out.println("Maximum element is: "+ max);5
+         
+    }
+}
