387c45e8e9f3081c27ede0c530fe435a2ecac2ea
swarnkarvikash <vikashswarnkar3@gmail.com>
2024-06-16 16:39:29 +0530

    Adding PairSum program . In PairSum problem sum of two value will be Target value And we return no. of pair

---
+++ Arrays/PairSum.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PairSum {
+
+    static int TwoSum(int[] arr, int target){
+        int n = arr.length;
+
+        int ans = 0;
+
+        for(int i =0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i] + arr[j] == target){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no. of elements: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter Elements in array");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter target: ");
+        int target = sc.nextInt();
+
+        
+       System.out.println(TwoSum(arr, target));
+       
+
+
+    }
+}
