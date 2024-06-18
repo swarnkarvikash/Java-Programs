1e408913f3528af4efbfe421e6c76b2987e202cc
swarnkarvikash <vikashswarnkar3@gmail.com>
2024-06-18 23:22:56 +0530

    initialize the PrefixSum program

---
+++ Arrays/PrefixSum.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class PrefixSum {
+
+    static int[] makeprefixSumArray(int arr[]){
+        int  n = arr.length;
+        int pref[] = new int[n];
+        pref[0] = arr[0];
+        for(int i=1;i<n;i++){
+            pref[i] = pref[i-1] + arr[i];
+        }
+        return pref;
+    }
+
+    static void printArray(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter "+n+" elements");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Input Array: ");
+        printArray(arr);
+
+        int pref[] = makeprefixSumArray(arr);
+        printArray(pref);
+
+    }
+
+}
