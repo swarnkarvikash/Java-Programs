123024d4ac7cdb1f9db50be22ef126e829a22e88
swarnkarvikash <vikashswarnkar3@gmail.com>
2024-06-25 20:43:03 +0530

    concept of class and object

---
+++ OOPS/Class and Object/Test.java
@@ -0,0 +1,15 @@
+class Demo {
+  int a=10;
+  String b ="Vikash";
+
+  void Show(){
+    System.out.print(a+" "+b);
+  }
+}
+
+public class Test{
+  public static void main(String[] args) {
+    Demo r = new Demo();
+    r.Show();
+  }
+}
