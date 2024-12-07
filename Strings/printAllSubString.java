55846bef079e2edebc93bfcc3fae5e8182e8df2f
swarnkarvikash <vikashswarnkar3@gmail.com>
2024-12-07 12:47:14 +0530

    print all substring of string

---
+++ Strings/printAllSubString.java
@@ -0,0 +1,15 @@
+//import java.util.*;
+
+public class printAllSubString {
+    public static void main(String[] args) {
+        String str = "abcd";
+
+        for(int i=0; i<=3; i++){
+
+            for(int j=i+1; j<=4; j++){
+
+                System.out.print(str.substring(i, j)+" ");
+            }
+        }
+    }
+}
