545bcfd48d069e8557f4f4fe3c6906fc5596b5eb
swarnkarvikash <vikashswarnkar3@gmail.com>
2024-12-07 11:36:00 +0530

    learned subsrting function

---
+++ Strings/subStr.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class subStr {
+    public static void main(String[] args){
+        String str = "abcde";
+        System.out.println(str.substring(0,2));
+
+        System.out.println(str.substring(1));
+    }
+}
