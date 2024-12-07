8482c0f4514e50202beb3df2599e5ed1a6724672
swarnkarvikash <vikashswarnkar3@gmail.com>
2024-12-07 09:41:18 +0530

    Learned basic function and operation of Strings in Java

---
+++ Strings/stringBasics.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class stringBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String str = "Vikash Swarnkar";
+        // System.out.println(str);
+
+       // String str = sc.next();
+        // String s = sc.nextLine();
+        // System.out.println(s);
+        // String str = "Hello";
+        // // int len = str.length();
+        // // System.out.println(len);
+        
+        // // char ch = str.charAt(4 );
+        // // System.out.println(ch);
+        // // System.out.println(str.indexOf("o"));
+
+        // String gtr = "Zello";
+        // System.out.println(str.compareTo((gtr)));
+
+        // String str = "Physics Wallah";
+        // // System.out.println(str.contains("alla"));
+        // // System.out.println(str.startsWith("Ph"));
+        // // System.out.println(str.endsWith("ah"));
+
+        // System.out.println(str.toLowerCase());
+        // System.out.println(str.toUpperCase());
+
+        String s1 = "abc";
+        String s2 = "def";
+        System.out.println(s1.concat(s2));
+    }
+}
