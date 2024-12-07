012a45a1f6a54419877a75062bd3084286ab8bf0
swarnkarvikash <vikashswarnkar3@gmail.com>
2024-12-07 11:34:51 +0530

    Addition of string with char, integer AND STRING

---
+++ Strings/stringIntChar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class stringIntChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String str = "abc";
+        // str = str+"def";
+        //System.out.println(str);
+        System.out.println("abc"+"xyz");
+
+        System.out.println("abc"+10+20);
+    }
+}
