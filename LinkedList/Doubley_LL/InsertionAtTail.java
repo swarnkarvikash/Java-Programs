31b481a1f4c6b51aac750f4292256d67866c4c4e
swarnkarvikash <vikashswarnkar3@gmail.com>
2025-01-06 09:40:05 +0530

    Insertion At Tail in Doubly Linked List

---
+++ LinkedList/Doubley_LL/InsertionAtTail.java
@@ -0,0 +1,71 @@
+ 
+
+public class InsertionAtTail {
+    public static void InsertionTail(Node head, int val){
+        Node t = new Node(val);
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = t;
+        t.prev = temp;
+        t.next = null;
+    }
+    public static class Node{
+        int val;
+        Node next;
+        Node prev;
+        Node(int val){
+            this.val = val;
+        }
+    }
+    public static void display(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void display2(Node tail){
+        Node temp = tail;
+        while(temp != null){
+            System.out.print(temp.val+" ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+    public static void display3(Node random){
+        Node temp = random;
+        while(temp.prev != null){
+            temp = temp.prev;
+        }
+        while(temp != null){
+            System.out.print(temp.val+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node a = new Node(4);
+        Node b = new Node(10);
+        Node c = new Node(2);
+        Node d = new Node(99);
+        Node e = new Node(13);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = null;
+        e.prev = d;
+        d.prev = c;
+        c.prev = b;
+        b.prev = a;
+        a.prev = null;
+        display(a);
+        display2(e);
+        display3(c);
+        InsertionTail(a,40);
+        display(a);
+    }
+}
